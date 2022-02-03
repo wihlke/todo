fc8f80f1436b9aef269f27db9e15ced980dbf1ed
Patrik Wihlke <patrik.wihlke@live.se>
2022-02-03 19:24:15 +0100

    Prepare for refresh token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import login
 
 urlpatterns = [
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/', login, name='login'),
 ]
